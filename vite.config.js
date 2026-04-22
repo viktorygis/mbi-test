acb17891a28248d63093b2028855457fba546cfa
viktorygis <gis07072016@gmail.com>
2026-04-22 13:24:25 +0300

    refactor: удалить неиспользуемые компоненты и улучшить конфигурацию Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/mbi-test/",
+  //base: "/mbi-test-build/",
   server: {
     host: "127.0.0.1",
     port: 5173,
